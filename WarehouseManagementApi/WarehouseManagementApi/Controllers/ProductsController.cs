6c0ed46fd190ee0d2c471bc00e639182339414a4
JimmyOrval <jimmy_orval@hotmail.com>
2026-07-01 16:50:08 +0300

    feat(Session-02): Added Fake Warehouse and First 3 Get Controller Methods

---
+++ WarehouseManagementApi/WarehouseManagementApi/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagementApi.Models;
+
+namespace WarehouseManagementApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public List<Product> GetProducts([FromQuery] bool? onlyAvailable = true)
+    {
+        var products = FakeWarehouseStore.Products
+            .OrderByDescending(p => p.CreatedAt);
+        return FakeWarehouseStore.Products;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProductById([FromRoute] string id)
+    {
+        if (id?.Length != 36)
+            throw new InvalidDataException();
+        
+        var product = FakeWarehouseStore.Products.FirstOrDefault(x => x.Id.Equals(id));
+
+        if (product == null)
+            return NotFound();
+        
+        return Ok(product);
+    }
+
+    [HttpGet("search")]
+    public IActionResult Search([FromQuery] string? name, [FromQuery] string? supplier)
+    {
+        var results = FakeWarehouseStore.Products
+            .Where(p => (string.IsNullOrWhiteSpace(name) || p.Name.Contains(name, StringComparison.OrdinalIgnoreCase)
+                && (string.IsNullOrWhiteSpace(supplier) || p.SupplierName.Contains(supplier, StringComparison.OrdinalIgnoreCase))))
+            .ToList();
+        return Ok(results);
+    }
+}
---
+++ WarehouseManagementApi/WarehouseManagementApi/FakeWarehouseStore.cs
@@ -0,0 +1,59 @@
+﻿using WarehouseManagementApi.Models;
+
+namespace WarehouseManagementApi;
+
+public class FakeWarehouseStore
+{
+    public static List<Product> Products { get; set; }
+
+    static FakeWarehouseStore()
+    {
+        Products =
+        [
+            new Product
+            {
+                Id = "93a46de1-6427-486c-bdca-5faaa11da0d8",
+                Name = "Laptop",
+                Sku = "LAPTOP-SKU",
+                Description = "HP Laptop",
+                Price = 1000.00m,
+                QuantityInStock = 10,
+                SupplierName = "Supplier1",
+                ExpiryDate = DateTime.Now.AddMonths(1),
+                IsArchived = false,
+                CreatedAt = DateTime.Now.AddDays(-1),
+                LastUpdatedAt = DateTime.Now
+            },
+            
+            new Product
+            {
+                Id = "0ab26dda-2a10-4241-8647-a0b86d56fcf0",
+                Name = "Mouse",
+                Sku = "MOUSE-SKU",
+                Description = "Razer Mouse",
+                Price = 100.00m,
+                QuantityInStock = 20,
+                SupplierName = "Supplier2",
+                ExpiryDate = DateTime.Now.AddMonths(2),
+                IsArchived = true,
+                CreatedAt = DateTime.Now.AddDays(-2),
+                LastUpdatedAt = DateTime.Now
+            },
+            
+            new Product
+            {
+                Id = "d53c35c2-f668-4191-9631-fcee3de9081d",
+                Name = "Keyboard",
+                Sku = "KEYBOARD-SKU",
+                Description = "HyperX Keyboard",
+                Price = 200.00m,
+                QuantityInStock = 30,
+                SupplierName = "Supplier3",
+                ExpiryDate = DateTime.Now.AddMonths(3),
+                IsArchived = false,
+                CreatedAt = DateTime.Now.AddDays(-3),
+                LastUpdatedAt = DateTime.Now
+            }
+        ];
+    }
+}
