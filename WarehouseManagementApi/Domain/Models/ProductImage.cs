76f233c39bb0af14a77fc19616ba01f2b76d9832
JimmyOrval <jimmy_orval@hotmail.com>
2026-07-07 03:22:04 +0300

    refactor(Session-03): Added Product Reference for Each Image Instance

--- WarehouseManagementApi/Domain/Models/ProductImage.cs
+++ WarehouseManagementApi/Domain/Models/ProductImage.cs
@@ -7,6 +7,9 @@ public class ProductImage
     [Required]
     public required string Id { get; set; }
     [Required]
+    public required string ProductId { get; set; }
+    public Product? Product { get; set; }
+    [Required]
     public required string FileName { get; set; }
     [Required]
     public required string FilePath { get; set; }
