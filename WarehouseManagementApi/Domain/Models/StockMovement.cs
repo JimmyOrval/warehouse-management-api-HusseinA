afa833fbecd23a9e16a03e3fff7abfa9ab66c31e
JimmyOrval <jimmy_orval@hotmail.com>
2026-07-07 03:19:07 +0300

    feat(Session-03): Introduced WarehouseItem and StockMovement Entities

---
+++ WarehouseManagementApi/Domain/Models/StockMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+
+namespace Domain.Models;
+
+public class StockMovement
+{
+    [Key]
+    public required string Id { get; set; }
+    
+    [Required(ErrorMessage = "Item ID is required")]
+    public required string WarehouseItemId { get; set; }
+    public WarehouseItem? WarehouseItem { get; set; }
+    
+    [Required(ErrorMessage = "Movement date is required")]
+    public DateTime MovementDate { get; set; }
+    
+    [Required(ErrorMessage = "Quantity is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Quantity cannot be negative")]
+    public int Quantity { get; set; }
+    
+    [Required]
+    public StockMovementType MovementType { get; set; }
+}
---
+++ WarehouseManagementApi/Domain/Models/WarehouseItem.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class WarehouseItem
+{
+    [Key]
+    public required string Id { get; set; }
+    
+    [Required(ErrorMessage = "Product connection is required")]
+    public required string ProductId { get; set; }
+    public Product? Product { get; set; }
+    
+    [Required(ErrorMessage = "Item location is required")]
+    public string Location { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Quantity is required")]
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity cannot be negative")]
+    public int QuantityInStock { get; set; } = 0;
+    
+    public DateTime LastStockUpdate { get; set; }
+    
+    public void StockIn(int quantity)
+    {
+        QuantityInStock += quantity;
+        LastStockUpdate = DateTime.Now;
+    }
+
+    public void StockOut(int quantity)
+    {
+        QuantityInStock -= quantity;
+        LastStockUpdate = DateTime.Now;
+    }
+}
