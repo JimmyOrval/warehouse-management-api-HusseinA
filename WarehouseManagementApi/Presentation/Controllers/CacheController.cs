4b9026045d12d2a6fdfcc6dcec49bffae15530b9
JimmyOrval <jimmy_orval@hotmail.com>
2026-07-18 23:15:34 +0300

    fix(Session-06): Added Missing CacheController

---
+++ WarehouseManagementApi/Presentation/Controllers/CacheController.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/cache")]
+public class CacheController(ICacheStatsTracker cacheStats) : ControllerBase
+{
+    [HttpGet("stats")]
+    public IActionResult GetStats() => Ok(cacheStats.GetStats());
+}
